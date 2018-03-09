6778f389d23ea9f11ecc099c6837a224f59ba1fc
jaejunmin <jaejunmi@usc.edu>
2018-03-08 23:43:21 -0800

    Initial Commit

---
+++ Homework4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homework4
+//
+//  Created by JaeJun Min on 08/03/2018.
+//  Copyright © 2018 JaeJun Min. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Homework4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Homework4
+//
+//  Created by JaeJun Min on 08/03/2018.
+//  Copyright © 2018 JaeJun Min. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
